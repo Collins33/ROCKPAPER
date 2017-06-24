e3e95eaa786371d6311f09784d8db65fd6ed451b
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-24 21:44:53 +0300

    added event handler for the form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+  $("form#playerinfo").submit(function(event){
+    event.preventDefault();
+    var name=$("input#name").val();
+    var age=parseInt($("input#age").val());
+    var year=2017;
+    if(year-age<18){
+      alert("cannot play")
+    }
+    $("#playername").append(name);
+
+  })
+
+
+
+
+
+
+
   var userscore=0;
   var computerscore=0;
   var userchoice="";
