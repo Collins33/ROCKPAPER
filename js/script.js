4c74c6c5a0d8588af9ed9c9f7f38e87c178f0fc7
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-25 02:01:33 +0300

    added functions for the score and comparison

--- js/script.js
+++ js/script.js
@@ -1,147 +1,85 @@
-$(document).ready(function(){
+$(document).ready(function() {
   /*event handler for the form*/
-  $("form#playerinfo").submit(function(event){
+  $("form#playerinfo").submit(function(event) {
     event.preventDefault();
-    var name=$("input#name").val();
-    var age=parseInt($("input#age").val());
-    var year=2017;
-    if(year-age<18){
+    var name = $("input#name").val();
+    var age = parseInt($("input#age").val());
+    var year = 2017;
+    if (year - age < 18) {
       alert("cannot play")
     }
     $("#playername").append(name);
 
   });
 
-  var userscore=0;
-  var computerscore=0;
-  var userchoice="";
-  var computerchoice=parseInt("");
-  /*click function for pictures and random number generator for computer*/
-  $("#firstpic").click(function(){
-  userchoice="rock";
-  computerchoice=Math.floor((Math.random() * 9) + 1);
-  if (computerchoice <3) {
-    computerchoice = "rock";
-  } else if (computerchoice <6) {
-    computerchoice = "paper";
-  } else {
-    computerchoice = "scissors";
-  };
-  if (userchoice === computerchoice) {
-      userscore+=0;computerscore+=0;
-  } else if (userchoice === "rock") {
+  var userscore = 0;
+  var computerscore = 0;
+  var userchoice = "";
+  var computerchoice = parseInt("");
+
+  var computerplay = function() {
+    computerchoice = Math.floor((Math.random() * 9) + 1);
+    if (computerchoice < 3) {
+      computerchoice = "rock";
+    } else if (computerchoice < 6) {
+      computerchoice = "paper";
+    } else {
+      computerchoice = "scissors";
+    };
+  }
+  var comparison = function() {
+    if (userchoice === computerchoice) {
+      userscore += 0;
+      computerscore += 0;
+    } else if (userchoice === "rock") {
       if (computerchoice === "scissors") {
-           userscore+=1;
+        userscore += 1;
       } else {
-           computerscore+=1;
+        computerscore += 1;
       };
-  } else if (userchoice == "paper") {
+    } else if (userchoice == "paper") {
       if (computerchoice == "rock") {
-           userscore+=1;
+        userscore += 1;
       } else {
-          computerscore+1;
+        computerscore += 1;
       };
-  } else if (userchoice == "scissors") {
+    } else if (userchoice == "scissors") {
       if (computerchoice == "paper") {
-          userscore+=1;
+        userscore += 1;
       } else {
-          computerscore+=1;
+        computerscore += 1;
       };
+    };
+  }
+  var results = function() {
+    $("#choice1").text("");
+    $("#choice1").append(userchoice);
+    $("#choice2").text("");
+    $("#choice2").append(computerchoice);
+    $("#playerscore").text("");
+    $("#playerscore").append(userscore);
+    $("#computerscore").text("");
+    $("#computerscore").append(computerscore);
   };
-  $("#choice1").text("");
-  $("#choice1").append(userchoice);
-  $("#choice2").text("");
-  $("#choice2").append(computerchoice);
-  $("#playerscore").text("");
-  $("#playerscore").append(userscore);
-  $("#computerscore").text("");
-  $("#computerscore").append(computerscore);
+  /*click function for pictures and random number generator for computer*/
+  $("#firstpic").click(function() {
+    userchoice = "rock";
+    computerplay();
+    comparison();
+    results();
+  });
+  $("#secondpic").click(function() {
+    userchoice = "paper";
+    computerplay();
+    comparison();
+    results();
+  });
+  $("#thirdpic").click(function() {
+    userchoice = "scissors";
+    computerplay();
+    comparison();
+    results();
 
   });
-  $("#secondpic").click(function(){
-    userchoice="paper";
-    computerchoice=Math.floor((Math.random() * 9) + 1);
-    if (computerchoice <3) {
-      computerchoice = "rock";
-    } else if (computerchoice <6) {
-      computerchoice = "paper";
-    } else {
-      computerchoice = "scissors";
-    };
-    if (userchoice === computerchoice) {
-        userscore+=0;computerscore+=0;
-    } else if (userchoice === "rock") {
-        if (computerchoice === "scissors") {
-             userscore+=1;
-        } else {
-             computerscore+=1;
-        };
-    } else if (userchoice == "paper") {
-        if (computerchoice == "rock") {
-             userscore+=1;
-        } else {
-            computerscore+=1;
-        };
-    } else if (userchoice == "scissors") {
-        if (computerchoice == "paper") {
-            userscore+=1;
-        } else {
-            computerscore+=1;
-        };
-    };
-    $("#choice1").text("");
-    $("#choice1").append(userchoice);
-    $("#choice2").text("");
-    $("#choice2").append(computerchoice);
-    $("#playerscore").text("");
-    $("#playerscore").append(userscore);
-    $("#computerscore").text("");
-    $("#computerscore").append(computerscore);
-  });
-  $("#thirdpic").click(function(){
-    userchoice="scissors";
-    computerchoice=Math.floor((Math.random() * 9) + 1);
-    if (computerchoice <3) {
-      computerchoice = "rock";
-    } else if (computerchoice <6) {
-      computerchoice = "paper";
-    } else {
-      computerchoice = "scissors";
-    };
-    if (userchoice === computerchoice) {
-        userscore+=0;computerscore+=0;
-    } else if (userchoice === "rock") {
-        if (computerchoice === "scissors") {
-             userscore+=1;
-        } else {
-             computerscore+=1;
-        };
-    } else if (userchoice == "paper") {
-        if (computerchoice == "rock") {
-             userscore+=1;
-        } else {
-            computerscore+1;
-        };
-    } else if (userchoice == "scissors") {
-        if (computerchoice == "paper") {
-            userscore+=1;
-        } else {
-            computerscore+=1;
-        };
-    };
-    $("#choice1").text("");
-    $("#choice1").append(userchoice);
-    $("#choice2").text("");
-    $("#choice2").append(computerchoice);
-    $("#playerscore").text("");
-    $("#playerscore").append(userscore);
-    $("#computerscore").text("");
-    $("#computerscore").append(computerscore);
-  });
-
-
-
-
-
 
 });
