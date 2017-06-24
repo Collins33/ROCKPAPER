48ca637303f93b346cf8bf3468498957c02f7b4d
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-24 21:30:48 +0300

    added script for when user clicks photo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var userscore=0;
+  var computerscore=0;
+  var userchoice="";
+  $("#firstpic").click(function(){
+  userchoice="rock";
+
+  });
+  $("#secondpic").click(function(){
+    userchoice="paper";
+  });
+  $("#thirdpic").click(function(){
+    userchoice="scissors"
+  });
+});
