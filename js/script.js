9f5805d795252e298fdda945e927483becedae94
collins njau <collinsnjau39@gmail.com>
2019-08-17 21:37:32 +0300

    Fix styling for main game - fix css - fix submit button - fix footer

--- js/script.js
+++ js/script.js
@@ -8,16 +8,14 @@ $(document).ready(function() {
     if (year - age < 18) {
       swal("AGE LIMIT NOT MET", "CHANCES ARE YOU ARE TOO YOUNG");
     } else if (year - age > 18) {
-      $("#maingame").show();
+      $(".maingame_row").show();
+      $(".proceed_button").hide();
       $("#mainresult").show();
       $("#playername").text("");
       $("#playername").append(name);
-      
-
+      swal("Congratulations!!. Close modal to proceed to the game");
     }
     window.scrollTo(0, 2000);
-
-
   });
 
   var userscore = 0;
@@ -26,15 +24,15 @@ $(document).ready(function() {
   var computerchoice = parseInt("");
 
   var computerplay = function() {
-    computerchoice = Math.floor((Math.random() * 9) + 1);
+    computerchoice = Math.floor(Math.random() * 9 + 1);
     if (computerchoice < 3) {
       computerchoice = "rock";
     } else if (computerchoice < 6) {
       computerchoice = "paper";
     } else {
       computerchoice = "scissors";
-    };
-  }
+    }
+  };
   var comparison = function() {
     if (userchoice === computerchoice) {
       userscore += 0;
@@ -44,21 +42,21 @@ $(document).ready(function() {
         userscore += 1;
       } else {
         computerscore += 1;
-      };
+      }
     } else if (userchoice == "paper") {
       if (computerchoice == "rock") {
         userscore += 1;
       } else {
         computerscore += 1;
-      };
+      }
     } else if (userchoice == "scissors") {
       if (computerchoice == "paper") {
         userscore += 1;
       } else {
         computerscore += 1;
-      };
-    };
-  }
+      }
+    }
+  };
   var results = function() {
     $("#choice1").text("");
     $("#choice1").append(userchoice);
@@ -74,19 +72,21 @@ $(document).ready(function() {
       swal({
         title: "YOU LOOSE!!!!!!!!!!",
         text: "GO PLAY SOMETHING ELSE!!!!!",
-        imageUrl: "http://i.huffpost.com/gen/2714370/images/o-POOP-EMOJI-ICE-CREAM-facebook.jpg"
+        imageUrl:
+          "http://i.huffpost.com/gen/2714370/images/o-POOP-EMOJI-ICE-CREAM-facebook.jpg"
       });
       $("#maingame").hide();
     } else if (userscore >= 30) {
       swal({
         title: "Sweet!!!!!!",
         text: "YOU ACTUALLY WON SOMETHING IN YOUR LIFE!!!",
-        imageUrl: "https://thumbs.dreamstime.com/z/thumbs-up-emoticon-vector-design-showing-73992493.jpg"
+        imageUrl:
+          "https://thumbs.dreamstime.com/z/thumbs-up-emoticon-vector-design-showing-73992493.jpg"
       });
 
       $("#maingame").hide();
     }
-  }
+  };
   /*click function for pictures and random number generator for computer*/
   $("#firstpic").click(function() {
     userchoice = "rock";
@@ -108,12 +108,5 @@ $(document).ready(function() {
     comparison();
     results();
     verdict();
-
   });
-
-
-
-
-
-
 });
